9d11954a26bd360ac5c57c316bfc6721a45739c9
Eddie C. <edimc2000@gmail.com>
2025-12-01 17:16:25 -0600

    organized files

---
+++ RegexPatternTesterUnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+using static DotNetA1Regex.AnsiColorCodes; 
+using static DotNetA1Regex.Utility; 
+
+namespace RegexPatternTesterUnitTests
+{
+    public class UnitTest1
+    {
+        [Fact(DisplayName = "Match a single character not present in the list below [^a-z]")]
+        public void Test1()
+        {
+            Regex regexPattern = new("[^a-z]+/g");
+
+            string actual = MatchPattern(regexPattern, "a");
+            string expectedTrue = $"{GreenOnWhite} ✅ True {Reset}";
+            string expectedFalse = $"{RedOnWhite} ❌ False {Reset}";
+
+            Assert.Equal(expectedFalse, actual);
+        }
+    }
+}
