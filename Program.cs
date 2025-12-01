c72fbd0843146a0b3d90368bc5ae097cd8aba32d
Eddie C. <edimc2000@gmail.com>
2025-11-30 19:54:12 -0600

    inital commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetA1Regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
